ce9319f181de42418e807da54b9e26bb26207e54
Trina Choudhury <trina.choudhury@1mg.com>
2022-09-14 02:05:20 +0530

    Base CSS Setup before eject

---
+++ 03-css-setup/src/components/Card/Card.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import "./Card.css"
+
+const Card = ({ url, image, header, description }) => (
+    <article className="card">
+        <Link to={url}>
+            {image && (
+                <figure className="thumbnail">
+                    <img src={`${image}`} alt="meow" />
+                </figure>
+            )}
+            {(header || description) && (
+                <div className="card-content">
+                    {header && <h2>{header}</h2>}
+                    {description && <p>{description}</p>}
+                </div>
+            )}
+        </Link>
+    </article>
+);
+
+export default Card;
---
+++ 03-css-setup/src/components/Loader/Loader.js
@@ -0,0 +1,14 @@
+import "./Loader.css";
+
+const Loader = () => (
+    <div className="loaderContainer">
+        <div className="loader">
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+        </div>
+    </div>
+);
+
+export default Loader;

eac0aaa0af0af0f4f124b537e74ef0491181d5cf
Trina Choudhury <trina.choudhury@1mg.com>
2022-09-14 02:05:20 +0530

    Base CSS Setup before eject

---
+++ 03-css-setup/src/App.js
@@ -0,0 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header/Header";
+import Breeds from "./pages/Breeds";
+import Dogs from "./pages/Dogs";
+import Dog from "./pages/Dog";
+
+import "./static/css/app.css";
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <main>
+                <Routes>
+                    <Route path="/" element={<Breeds />} />
+                    <Route path="/dogs/:breed" element={<Dogs />} />
+                    <Route path="/:name" element={<Dog />} />
+                </Routes>
+            </main>
+        </div>
+    );
+}
+
+export default App;

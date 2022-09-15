dec025766a7d76fcde6a49a903004e63b3f681da
Trina Choudhury <trina.choudhury@1mg.com>
2022-09-15 19:27:49 +0530

    Serve from public folder in case of dev mode, Overriden Webpack config using customize CRA

--- 03-css-setup/server/index.js
+++ 03-css-setup/server/index.js
@@ -6,7 +6,8 @@ const cors = require('cors');
 
 const app = express();
 
-const PublicFolder = path.resolve(__dirname, process.env === "production" ? "../build" : "../public");
+
+const PublicFolder = path.resolve(__dirname, process.env.env !== "dev" ? "../build" : "../public");
 
 if (process.env.env !== "dev")
   app.use(favicon(path.join(__dirname, "../build", "favicon.ico")));

40ac7165d28ea1b5c8a8c5bd1e87e58179e8b14d
geekayush <srivastavs61@gmail.com>
2022-09-18 14:46:13 +0530

    Clean up 01-basic-app-setup

--- 01-basic-app-setup/server/index.js
+++ 01-basic-app-setup/server/index.js
@@ -26,6 +26,6 @@ app.use("*", (req, res) => {
   res.sendFile(`${PublicFolder}/index.html`)
 })
 
-app.listen(3003, () => {
-  console.log("server started on port 3003");
+app.listen(3001, () => {
+  console.log("server started on port 3001");
 });
